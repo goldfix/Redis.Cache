d7c53e4597e75e6d91afdb4db63dd942825a361a
goldfix <decpack@goldfix.it>
2014-04-27 13:08:27 +0200

    Rename file (old: Exception)

---
+++ Redis.Cache/RedisCacheException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Redis.Cache
+{
+    public class RedisCacheException:Exception
+    {
+        /// <summary>
+        /// Custom Redis.Cache Exception 
+        /// </summary>
+        /// <param name="mex"></param>
+        /// <param name="inner"></param>
+        public RedisCacheException(string mex, Exception inner)
+            : base(mex, inner)
+        { }
+    }
+}
