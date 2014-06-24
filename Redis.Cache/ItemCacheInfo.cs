be05522a1cac953e2e80206596620339395aa130
goldfix <decpack@goldfix.it>
2014-06-24 11:38:11 +0200

    deleted constructor and review SlidingExpiration_TS function

--- Redis.Cache/ItemCacheInfo.cs
+++ Redis.Cache/ItemCacheInfo.cs
@@ -1,4 +1,31 @@
-﻿using System;
+﻿/*
+Copyright (c) 2014, pietro partescano
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the Redis.Cache.Py nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,23 +47,6 @@ namespace Redis.Cache
         public ItemCacheInfo()
         {
             _Init();
-        }
-
-        public ItemCacheInfo(string info, T data)
-        {
-            _Init();
-
-            TimeSpan[] ttl_Ts = Utility.TTL_TS_DeSerialize(info);
-            DateTime[] ttl_Dt = Utility.TTL_DT_DeSerialize(info);
-
-            this.SlidingExpiration_TS = ttl_Ts[0];
-            this.AbsoluteExpiration_TS = ttl_Ts[1];
-            this.SlidingExpiration_DT = ttl_Dt[0];
-            this.AbsoluteExpiration_DT = ttl_Dt[1];
-
-            this.StatusItem = Utility.StatusItemDeSerialize(info);
-
-            this.Data = data;
         }
 
         public TimeSpan SlidingExpiration_TS
@@ -67,7 +77,6 @@ namespace Redis.Cache
             this.StatusItem = _StatusItem;
 
             this.UpdateSerialized_TTL();    //Update TTL and generate new ttl string.
-            this.Serialized_TTL = this.Serialized_TTL;
         }
         public void DeSerializeInfo()
         {
